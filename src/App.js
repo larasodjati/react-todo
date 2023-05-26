a9aad73c422408eb4e8bd5c236ba83e14eee8420
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-05-25 23:52:21 -0700

    Changed configuration on prettier

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
 const todoList = [
-    {
-        id: 1,
-        title: 'Washing dishes',
-    },
-    {
-        id: 2,
-        title: 'Feeding cats',
-    },
-    {
-        id: 3,
-        title: 'Morning walk',
-    },
+  {
+    id: 1,
+    title: 'Washing dishes'
+  },
+  {
+    id: 2,
+    title: 'Feeding cats'
+  },
+  {
+    id: 3,
+    title: 'Morning walk'
+  }
 ];
 
 function App() {
-    return (
-        <div>
-            <h1>Todo List</h1>
-            <ul>
-                {todoList.map(function (item) {
-                    return <li key={item.id}>{item.title}</li>;
-                })}
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {todoList.map(function (item) {
+          return <li key={item.id}>{item.title}</li>;
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
