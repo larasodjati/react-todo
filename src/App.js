84e2ca28f7835623dbf0994bd5d6fb7c78eb0c47
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-06-05 23:50:55 -0700

    Moved list into new TodoList component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import React from 'react';
-
-const todoList = [
-  {
-    id: 1,
-    title: 'Washing dishes'
-  },
-  {
-    id: 2,
-    title: 'Feeding cats'
-  },
-  {
-    id: 3,
-    title: 'Morning walk'
-  }
-];
+import TodoList from './TodoList';
 
 function App() {
   return (
     <div>
       <h1>Todo List</h1>
-      <ul>
-        {todoList.map(function (item) {
-          return <li key={item.id}>{item.title}</li>;
-        })}
-      </ul>
+      <TodoList />
     </div>
   );
 }
