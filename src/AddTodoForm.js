ac9da09af2277b4ac2fadbf596a7f97a9ccd567c
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-06-14 00:23:45 -0700

    Invoke onAddTodo callback inside handleAddTodo function in AddTodoForm component

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-function AddTodoForm() {
+function AddTodoForm(props) {
   const handleAddTodo = (event) => {
-    event.preventDefault();
     const todoTitle = event.target.title.value;
+    props.onAddTodo(todoTitle);
     console.log(todoTitle);
+    event.preventDefault();
     event.target.reset();
   };
   return (
