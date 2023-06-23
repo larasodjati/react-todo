aa708c99fa58835e3c04c8a75a96dc86ece87508
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-06-23 13:00:47 -0700

    Add todoTitle state and handleTitleChange event into addTodoForm component

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 function AddTodoForm(props) {
+  const [todoTitle, setTodoTitle] = React.useState('');
+
+  const handleTitleChange = (event) => {
+    const newTodoTitle = event.target.value;
+    setTodoTitle(newTodoTitle);
+  };
   const handleAddTodo = (event) => {
     event.preventDefault();
     const todoTitle = event.target.title.value;
@@ -11,7 +17,13 @@ function AddTodoForm(props) {
   return (
     <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Title</label>
-      <input type="text" id="todoTitle" name="title"></input>
+      <input
+        type="text"
+        id="todoTitle"
+        name="title"
+        value={todoTitle}
+        onChange={handleTitleChange}
+      ></input>
       <button>Add</button>
     </form>
   );
