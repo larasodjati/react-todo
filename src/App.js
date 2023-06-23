f5f93db136f559b47bc07cfc6451f90b288eb339
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-06-23 13:16:13 -0700

    Remove newTodo state and its corresponding JSX in the App component

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
 function App() {
-  const [newTodo, setNewTodo] = React.useState('');
   const [todoList, setTodoList] = React.useState([]);
   return (
     <div>
       <h1>Todo List</h1>
-      <AddTodoForm onAddTodo={setNewTodo} />
+      <AddTodoForm onAddTodo={} />
       <p>
-        New thing to do is <strong>{newTodo}</strong>
+        New thing to do is <strong>{}</strong>
       </p>
       <TodoList todoList={todoList} />
     </div>
