0d5008c84d4d5a9a65442d39c51966640106d1fe
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-07-13 23:11:26 -0700

    Replace label prop with children so that child node can be used as the label text

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -3,7 +3,7 @@ import React from 'react';
 function InputWithLabel(props) {
   return (
     <>
-      <label htmlFor="todoTitle">{props.label}</label>
+      <label htmlFor="todoTitle">{props.children}</label>
       <input
         type="text"
         id="todoTitle"
