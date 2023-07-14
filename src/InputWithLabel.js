ce2d18dcb1a4fcc210f5a56d14285fafe3972820
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-07-13 23:53:25 -0700

    Create auto focus input by refactoring the InputWithLabel component using useRef, useEffectHooks, and focus inputRef

--- src/InputWithLabel.js
+++ src/InputWithLabel.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 function InputWithLabel(props) {
+  const inputRef = React.useRef();
+
+  React.useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <>
       <label htmlFor="todoTitle">{props.children}</label>
       <input
+        ref={inputRef}
         type="text"
         id="todoTitle"
         name="title"
         value={props.todoTitle}
-        autoFocus
         onChange={props.handleTitleChange}
       />
     </>
