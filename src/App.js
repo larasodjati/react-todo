3f7136a8801ae1ee157ca129fe745fe261a084b2
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-07-24 13:39:58 -0700

    Remove useSemiPersistentState custom hook and move useState and useEffect hook back into App

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import React from 'react';
 import AddTodoForm from './AddTodoForm';
 import TodoList from './TodoList';
 
-const useSemiPersistentState = () => {
+// const useSemiPersistentState = () => {
+//   const [todoList, setTodoList] = React.useState(
+//     JSON.parse(localStorage.getItem('savedTodoList')) || []
+//   );
+
+//   React.useEffect(() => {
+//     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+//   }, [todoList]);
+
+//   return [todoList, setTodoList];
+// };
+
+function App() {
   const [todoList, setTodoList] = React.useState(
     JSON.parse(localStorage.getItem('savedTodoList')) || []
   );
@@ -10,12 +22,6 @@ const useSemiPersistentState = () => {
   React.useEffect(() => {
     localStorage.setItem('savedTodoList', JSON.stringify(todoList));
   }, [todoList]);
-
-  return [todoList, setTodoList];
-};
-
-function App() {
-  const [todoList, setTodoList] = useSemiPersistentState();
   const [removedTodo, setRemovedTodo] = React.useState(null);
 
   const addTodo = (newTodo) => {
