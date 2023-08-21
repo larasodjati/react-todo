05424a94b00bf639a13eb211948c8b9077041a74
larasodjati <108501073+larasodjati@users.noreply.github.com>
2023-08-21 00:19:02 -0700

    Update InputWithLabel component by defining the props and adding PropTypes

--- src/components/InputWithLabel.js
+++ src/components/InputWithLabel.js
@@ -1,25 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-function InputWithLabel(props) {
-  const inputRef = React.useRef();
+function InputWithLabel({ todoTitle, handleTitleChange, children }) {
+  const inputRef = useRef();
 
-  React.useEffect(() => {
+  useEffect(() => {
     inputRef.current.focus();
   }, []);
 
   return (
     <>
-      <label htmlFor="todoTitle">{props.children}</label>
+      <label htmlFor="todoTitle">{children}</label>
       <input
         ref={inputRef}
         type="text"
         id="todoTitle"
         name="title"
-        value={props.todoTitle}
-        onChange={props.handleTitleChange}
+        value={todoTitle}
+        onChange={handleTitleChange}
       />
     </>
   );
 }
 
+InputWithLabel.propTypes = {
+  todoTitle: PropTypes.string,
+  handleTitleChange: PropTypes.func,
+  children: PropTypes.node
+};
 export default InputWithLabel;
